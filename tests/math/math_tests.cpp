94dbd12fe868a8ff31e24f1c72b793beb8c09586
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2023-11-30 15:51:18 +0100

    added tests
    I'm so dumb. I was completely dumbfounded why it couldn't find gtest/gtest.h....... And it was just automatically added to the sources of the bored_engine target..... ffs

--- tests/math/math_tests.cpp
+++ tests/math/math_tests.cpp
@@ -4,8 +4,13 @@
 
 #include <iostream>
 
-int main( int argc, char* argv[] )
+//#include "../../src/math/vector.h"
+
+#include "gtest/gtest.h"
+
+int main(int argc, char* argv[])
 {
 	std::cout << "hello tests" << std::endl;
-	return 0;
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
