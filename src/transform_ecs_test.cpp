d84e0353587e79ed5895b50299bf31eef2a82832
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2023-12-29 15:16:04 +0100

    Update transform_ecs_test.cpp

--- src/transform_ecs_test.cpp
+++ src/transform_ecs_test.cpp
@@ -34,7 +34,8 @@ struct TransformComponent
 	bool dirty{true};
 
 	mat4 localToParentTransform{mat4::identity}; // from local space to parent space
-	mat4 localToWorldTransform{mat4::identity}; // from local space to world space (with parent's transformations applied)
+	mat4 localToWorldTransform{
+		mat4::identity}; // from local space to world space (with parent's transformations applied)
 
 	size_t parentIndex{0};
 };
@@ -46,9 +47,17 @@ struct Registry
 
 	void createEntity()
 	{
-		entities.emplace_back(Entity{.name = "Entity" });
+		entities.emplace_back(Entity{.name = "Entity"});
+	}
+
+	template<class Type>
+	void registerComponent()
+	{
+
 	}
 };
+// https://github.com/skypjack/entt/commits/master/?after=f2c417435c021b6bd2e426ffd9d1fc116b804146+5000
+// https://github.com/skypjack/entt/blob/ccf8f712ccbb32b2ac19e1b0f75ac0630db71caf/src/registry.hpp
 
 void update(Registry& registry)
 {
@@ -57,7 +66,8 @@ void update(Registry& registry)
 		if (t.dirty)
 		{
 			// update the localToParentTransform
-			t.localToParentTransform = math::createTranslationRotationScaleMatrix(t.localPosition, t.localRotation, t.localScale);
+			t.localToParentTransform = math::createTranslationRotationScaleMatrix(t.localPosition, t.localRotation,
+																				  t.localScale);
 			t.dirty = false;
 		}
 
