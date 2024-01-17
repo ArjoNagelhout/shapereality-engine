fbb872e8ed9d7a3f5268f6db3464f4770fb13556
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2024-01-17 02:18:36 +0100

    and forgot to check

--- src/modules/entity/view.h
+++ src/modules/entity/view.h
@@ -169,7 +169,7 @@ namespace entity
                     // select the component type with the smallest denseSize
                     view = std::get<0>(components);
                     std::apply([&_view = view](auto* ...component) {
-                        ((_view = (component ? component->denseSize() : 0) < _view->denseSize() ? component : _view), ...);
+                        ((_view = (component ? component->denseSize() : 0) < (_view ? _view->denseSize() : 0) ? component : _view), ...);
                     }, components);
                     break;
             }
