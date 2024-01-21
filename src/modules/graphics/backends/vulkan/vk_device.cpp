b995ed26b30bf45750961d008cacbdedb4ff0586
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2024-01-21 04:04:21 +0100

    add vulkan implementation

--- src/modules/graphics/backends/vulkan/vk_device.cpp
+++ src/modules/graphics/backends/vulkan/vk_device.cpp
@@ -4,9 +4,59 @@
 
 #include "vk_device.h"
 
+#include <cassert>
+
 namespace graphics
 {
     VulkanDevice::VulkanDevice() = default;
 
     VulkanDevice::~VulkanDevice() = default;
+
+    std::unique_ptr<Window>
+    VulkanDevice::createWindow(WindowDescriptor const& descriptor) const
+    {
+        assert(false && "Vulkan is not implemented");
+    }
+
+    std::unique_ptr<ICommandQueue>
+    VulkanDevice::createCommandQueue(CommandQueueDescriptor const& descriptor) const
+    {
+        assert(false && "Vulkan is not implemented");
+    }
+
+    std::unique_ptr<IRenderPass>
+    VulkanDevice::createRenderPass(RenderPassDescriptor const& descriptor) const
+    {
+        assert(false && "Vulkan is not implemented");
+    }
+
+    std::unique_ptr<IRenderPipelineState>
+    VulkanDevice::createRenderPipelineState(RenderPipelineDescriptor const& descriptor) const
+    {
+        assert(false && "Vulkan is not implemented");
+    }
+
+    std::unique_ptr<IDepthStencilState>
+    VulkanDevice::createDepthStencilState(DepthStencilDescriptor const& descriptor) const
+    {
+        assert(false && "Vulkan is not implemented");
+    }
+
+    std::unique_ptr<IShaderLibrary>
+    VulkanDevice::createShaderLibrary(std::filesystem::path const& path) const
+    {
+        assert(false && "Vulkan is not implemented");
+    }
+
+    std::unique_ptr<IBuffer>
+    VulkanDevice::createBuffer(BufferDescriptor const& descriptor) const
+    {
+        assert(false && "Vulkan is not implemented");
+    }
+
+    std::unique_ptr<ITexture>
+    VulkanDevice::createTexture(TextureDescriptor const& descriptor) const
+    {
+        assert(false && "Vulkan is not implemented");
+    }
 }
