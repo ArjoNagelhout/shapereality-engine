14741214d22026edb2aefc90a203f01c069f420b
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2024-04-05 18:18:33 +0200

    additional notes for polymorphic reflection

--- tests/reflection/polymorphism.cpp
+++ tests/reflection/polymorphism.cpp
@@ -52,12 +52,13 @@ namespace polymorphism_test
     };
 
     template<typename Type>
-    bool isType(std::any value) noexcept
+    bool isType(std::any value)
     {
-        return std::any_cast<Type*>(value);
+        Type** v = std::any_cast<Type>(&value);
+        return v != nullptr && *v != nullptr;
     }
 
-    TEST(Polymorphism, Test)
+    struct TypeInfoRegistry
     {
         std::unordered_map<TypeId, TypeInfo> types{
             {
@@ -91,6 +92,43 @@ namespace polymorphism_test
                 }
             }
         };
+
+        // get polymorphic type (subclass)
+        // std::any value = pointer to value
+        // id = base type
+        [[nodiscard]] TypeId getSubType(std::any value, TypeId id)
+        {
+            if (!types.contains(id))
+            {
+                return nullType;
+            }
+
+            TypeInfo& base = types.at(id);
+
+            for (auto& childTypeId: base.children)
+            {
+                // for each child, check if it is that type, and if so, recurse
+                TypeInfo& child = types.at(childTypeId);
+                if (child.isType(value))
+                {
+                    return getSubType(value, childTypeId);
+                }
+            }
+
+            // none of the children's isType (dynamic_cast) succeeded
+            return id;
+        }
+
+        template<typename Type>
+        [[nodiscard]] TypeId getSubType(std::any value)
+        {
+            return getSubType(value, id<Type>());
+        }
+    };
+
+    TEST(Polymorphism, Test)
+    {
+        TypeInfoRegistry r;
         std::vector<std::unique_ptr<Base>> items;
         items.emplace_back(std::make_unique<Child2>());
         items.emplace_back(std::make_unique<Child1>());
@@ -101,24 +139,67 @@ namespace polymorphism_test
         for (int i = 0; i < items.size(); i++)
         {
             std::unique_ptr<Base>& item = items[i];
+            std::cout << "item " << i << ": " << item << std::endl;
 
             // dynamic_cast to a pointer type returns nullptr if not valid
 
             std::any a = item.get();
 
-            if (dynamic_cast<Child1*>(item.get()))
+            Base** aa = std::any_cast<Base*>(&a);
+            assert(aa);
+            Base* o = *aa;
+
+            if (dynamic_cast<Child1*>(o))
             {
-                std::cout << "is child 1" << std::endl;
+                std::cout << "child1" << std::endl;
             }
-            else if (dynamic_cast<Child2*>(item.get()))
+            else if (dynamic_cast<Child2*>(o))
             {
-                std::cout << "is child 2" << std::endl;
+                std::cout << "child2" << std::endl;
             }
             else
             {
-                std::cout << "is base" << std::endl;
+                std::cout << "base" << std::endl;
             }
-            std::cout << "item" << item << std::endl;
         }
     }
+
+    TEST(A, B) {
+        std::any a = std::make_any<Child1*>(new Child1); // Storing a pointer to Child1 in std::any
+
+        // Attempt to cast using a pointer to the std::any instance to avoid exceptions
+        auto ptr = std::any_cast<Child1*>(&a);
+        if (ptr) {
+            std::cout << "a contains a pointer to Child1." << std::endl;
+        } else {
+            std::cout << "a does not contain a pointer to Child1." << std::endl;
+        }
+
+        // If ptr is not null, it means the cast was successful, and 'a' did contain a pointer to Child1.
+        // Remember to manage memory properly if you dynamically allocate memory for Child1.
+        // In this example, we're assuming ownership is elsewhere, so no delete is called here.
+
+
+    }
+
+    // problem:
+    // in order to perform a dynamic_cast we need to first do an any_cast
+    // to the base type. However, we're not sure how far down we could do a base
+    // cast
+    //
+    // e.g. we might have:
+    // struct Base {}
+    // struct Child : public Base {}
+    // struct Child2 : public Child {}
+    // struct Child3 : public Child2 {}
+    //
+    // if we then have a
+    // std::vector<std::unique_ptr<Child2>>
+    // we can't base cast to the top most class
+    // as that would throw a bad_any_cast exception
+    //
+    // however, we might be able to solve this
+    // by simply using any_cast, without having
+    // to use dynamic_cast, by using the
+    // noexcept version of the method.
 }
