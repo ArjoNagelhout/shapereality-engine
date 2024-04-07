01ae17fc834dfa1ae2c2442ffc1f5b297dca03ea
Arjo Nagelhout <arjo.nagelhout@gmail.com>
2024-04-07 22:49:31 +0200

    add polymorphism test

--- tests/reflection/polymorphism.cpp
+++ tests/reflection/polymorphism.cpp
@@ -4,199 +4,129 @@
 
 #include <gtest/gtest.h>
 
-namespace polymorphism_test
+#include <reflection/type_info.h>
+
+using namespace reflection;
+
+namespace polymorphism
 {
     struct Base
     {
         virtual ~Base() = default;
     };
 
-    struct Child1 : public Base
+    struct Child : public Base
     {
-
     };
 
-    struct Child2 : public Base
+    struct Child2 : public Child
     {
-
     };
 
-    using TypeId = size_t;
-
-    struct TypeIndex
+    struct Child3 : public Child2
     {
-        [[nodiscard]] static TypeId getNextId()
-        {
-            static TypeId value = 1;
-            return value++; // first return, then increment
-        }
     };
 
-    constexpr TypeId nullType = 0;
-
-    // should be wrapped inside struct to not have internal linkage
-    template<typename Type>
-    [[nodiscard]] TypeId id()
+    struct Child3_1 : public Child3
     {
-        static const TypeId value = TypeIndex::getNextId();
-        return value;
-    }
-
-    struct TypeInfo
-    {
-        std::string name;
-        TypeId base = nullType;
-        std::vector<TypeId> children{};
-
-        bool (* isType)(std::any);
     };
 
-    template<typename Type, typename BaseType>
-    bool isType(std::any value)
+    struct Child3_2 : public Child3
     {
-        auto** v = std::any_cast<BaseType*>(&value);
-        assert(v);
-        return dynamic_cast<Type*>(*v);
-    }
-
-    // dynamic cast requires the input value to be of the base type, because
-    // otherwise it doesn't have a reference to the vtable, and thus no information
-    // about the type hierarchy.
-    //
-    // The issue is that only the PointerNode knows which base type exists, but it can only store
-    // a generic function that takes std::any as arguments or has std::any as return value
-    //
-    // the std::any cast is what should be done by PointerNode
-
-    // pointer node knows to which base node we need to cast
-    // so the function pointer it contains should provide the conversion
-    // to the base type
-    // however, we can only do this generically
-
-    struct TypeInfoRegistry
-    {
-        std::unordered_map<TypeId, TypeInfo> types{
-            {
-                id<Base>(),
-                TypeInfo{
-                    .name = "Base",
-                    .base = nullType,
-                    .children = {
-                        id<Child1>(),
-                        id<Child2>()
-                    },
-                    .isType = isType<Base, Base>
-                }
-            },
-            {
-                id<Child1>(),
-                TypeInfo{
-                    .name = "Child1",
-                    .base = id<Base>(),
-                    .children = {
-
-                    },
-                    .isType = isType<Child1, Base>
-                }
-            },
-            {
-                id<Child2>(),
-                TypeInfo{
-                    .name = "Child2",
-                    .base = id<Base>(),
-                    .children = {
-
-                    },
-                    .isType = isType<Child2, Base>
-                }
-            }
-        };
-
-        // get polymorphic type (subclass)
-        // std::any value = pointer to value
-        // id = base type
-        [[nodiscard]] TypeId getChildType(std::any value, TypeId id)
-        {
-            if (!types.contains(id))
-            {
-                return nullType;
-            }
-
-            TypeInfo& base = types.at(id);
-
-            for (auto& childTypeId: base.children)
-            {
-                // for each child, check if it is that type, and if so, recurse
-                TypeInfo& child = types.at(childTypeId);
-                if (child.isType(value))
-                {
-                    return getChildType(value, childTypeId);
-                }
-            }
-
-            // none of the children's isType (dynamic_cast) succeeded
-            return id;
-        }
-
-        template<typename Type>
-        [[nodiscard]] TypeId getChildType(Type* value)
-        {
-            return getChildType(value, id<Type>());
-        }
     };
 
-    TEST(Polymorphism, Test)
+    struct Container1
+    {
+        std::vector<std::unique_ptr<Base>> items; // either Base, Child, Child2 or Child3
+    };
+
+    struct Container2
+    {
+        std::unique_ptr<Child2> someItem; // either Child2 or Child3
+    };
+
+    struct Container3
+    {
+        std::unordered_map<std::string, std::unique_ptr<Child>> items; // either Child, Child2 or Child3
+    };
+
+    TEST(Reflection, Polymorphism)
     {
         TypeInfoRegistry r;
-        std::vector<std::unique_ptr<Base>> items;
-        items.emplace_back(std::make_unique<Child2>());
-        items.emplace_back(std::make_unique<Child1>());
-        items.emplace_back(std::make_unique<Base>());
-        items.emplace_back(std::make_unique<Base>());
-        items.emplace_back(std::make_unique<Child2>());
 
-        for (int i = 0; i < items.size(); i++)
+        TypeInfoBuilder<Base>("Base")
+            .emplace(r);
+
+        TypeInfoBuilder<Child>("Child")
+            .base<Base>()
+            .emplace(r);
+
+        TypeInfoBuilder<Child2>("Child2")
+            .base<Child>()
+            .emplace(r);
+
+        TypeInfoBuilder<Child3>("Child3")
+            .base<Child2>()
+            .emplace(r);
+
+        TypeInfoBuilder<Child3_1>("Child3_1")
+            .base<Child3>()
+            .emplace(r);
+
+        TypeInfoBuilder<Child3_2>("Child3_2")
+            .base<Child3>()
+            .emplace(r);
+
+        TypeInfoBuilder<Container1>("Container1")
+            .property<&Container1::items>("items")
+            .emplace(r);
+
+        TypeInfoBuilder<Container2>("Container2")
+            .property<&Container2::someItem>("someItem")
+            .emplace(r);
+
+        TypeInfoBuilder<Container3>("Container3")
+            .property<&Container3::items>("items")
+            .emplace(r);
+
+        Container2 c;
+        c.someItem = std::make_unique<Child3_2>();
+
+        TypeId id = r.getChildType(c.someItem.get());
+        ASSERT_EQ(id, TypeIndex<Child3_2>::value());
+        TypeInfo* info = r.get(id);
+
+        std::cout << "child type is " << info->name << std::endl;
+
+        Container1 c1;
+        c1.items.emplace_back(std::make_unique<Base>());
+        c1.items.emplace_back(std::make_unique<Child>());
+        c1.items.emplace_back(std::make_unique<Child2>());
+        c1.items.emplace_back(std::make_unique<Child3>());
+        c1.items.emplace_back(std::make_unique<Child3_1>());
+        c1.items.emplace_back(std::make_unique<Child3_2>());
+        c1.items.emplace_back(std::make_unique<Child>());
+
+        std::vector<TypeId> targets{
+            TypeIndex<Base>::value(),
+            TypeIndex<Child>::value(),
+            TypeIndex<Child2>::value(),
+            TypeIndex<Child3>::value(),
+            TypeIndex<Child3_1>::value(),
+            TypeIndex<Child3_2>::value(),
+            TypeIndex<Child>::value()
+        };
+
+        for (size_t i = 0; i < c1.items.size(); i++)
         {
-            std::unique_ptr<Base>& item = items[i];
-            std::cout << "item " << i << ": " << item << std::endl;
+            Base* item = c1.items[i].get();
+            TypeId target = targets[i];
 
-            TypeId id = r.getChildType<Base>(item.get());
-            std::cout << "getChildType returned " << r.types[id].name << std::endl;
+            TypeId itemId = r.getChildType(item);
+            ASSERT_EQ(itemId, target);
+
+            TypeInfo* itemInfo = r.get(itemId);
+            std::cout << "item type is: " << itemInfo->name << std::endl;
         }
     }
-
-    // dynamic_cast to a pointer type returns nullptr if not valid
-
-    // problem:
-    // in order to perform a dynamic_cast we need to first do an any_cast
-    // to the base type. However, we're not sure how far down we could do a base
-    // cast
-    //
-    // e.g. we might have:
-    // struct Base {}
-    // struct Child : public Base {}
-    // struct Child2 : public Child {}
-    // struct Child3 : public Child2 {}
-    //
-    // if we then have a
-    // std::vector<std::unique_ptr<Child2>>
-    // we can't base cast to the top most class
-    // as that would throw a bad_any_cast exception
-    //
-    // however, we might be able to solve this
-    // by simply using any_cast, without having
-    // to use dynamic_cast, by using the
-    // noexcept version of the method.
-    //
-    // no, that doesn't work, because any_cast returns false if the pointer that was stored in std::any
-    // is of the base type, and we try to do an any_cast to the child type.
-    // so we do *need* dynamic_cast. Because that's the only way to perform runtime polymorphism checking.
-    //
-    // we also *need* any_cast, because dynamic_cast does not work on void*, it only works if we have a pointer
-    // to the base type.
-    // for each parent type we would need to have a separate is type function, so that we can cast to the base type.
-    // this is actually doable with the Pointer type node.
-    // so a pointer type node contains an isType function pointer with instantiated template of isType with the correct
-    // base type for that specific pointer node.
-
 }
